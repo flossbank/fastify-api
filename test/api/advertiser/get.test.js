414fc1fec25fdd4083f86221188858bfe1af3b95
Peter Squicciarini <stripedpajamas273@gmail.com>
2019-12-06 13:56:37 -0800

    Refactor /advertiser/get as an integ test

--- test/api/advertiser/get.test.js
+++ test/api/advertiser/get.test.js
@@ -1,5 +1,16 @@
 const test = require('ava')
-const { beforeEach, afterEach } = require('../../helpers/_setup')
+const { before, beforeEach, afterEach, after } = require('../../helpers/_setup')
+
+test.before(async (t) => {
+  await before(t, async (t, db) => {
+    const advertiserId1 = await db.createAdvertiser({
+      name: 'Honesty',
+      email: 'honey@etsy.com',
+      password: 'beekeeperbookkeeper'
+    })
+    t.context.advertiserId1 = advertiserId1.toHexString()
+  })
+})
 
 test.beforeEach(async (t) => {
   await beforeEach(t)
@@ -9,11 +20,15 @@ test.afterEach(async (t) => {
   await afterEach(t)
 })
 
+test.after(async (t) => {
+  await after(t)
+})
+
 test.failing('GET `/advertiser/get` 401 unauthorized', async (t) => {
   const res = await t.context.app.inject({
     method: 'GET',
     url: '/advertiser/get',
-    query: { advertiserId: 'test-advertiser-0' },
+    query: { advertiserId: t.context.advertiserId1 },
     headers: { authorization: 'invalid token' }
   })
   t.deepEqual(res.statusCode, 401)
@@ -23,13 +38,17 @@ test('GET `/advertiser/get` 200 success', async (t) => {
   const res = await t.context.app.inject({
     method: 'GET',
     url: '/advertiser/get',
-    query: { advertiserId: 'test-advertiser-0' },
+    query: { advertiserId: t.context.advertiserId1 },
     headers: { authorization: 'valid-session-token' }
   })
   t.deepEqual(res.statusCode, 200)
   t.deepEqual(JSON.parse(res.payload), {
     success: true,
-    advertiser: await t.context.db.getAdvertiser()
+    advertiser: {
+      id: t.context.advertiserId1,
+      name: 'Honesty',
+      email: 'honey@etsy.com'
+    }
   })
 })
 
@@ -44,7 +63,7 @@ test('GET `/advertiser/get` 400 bad request', async (t) => {
 })
 
 test('GET `/advertiser/get` 500 server error', async (t) => {
-  t.context.db.getAdvertiser.throws()
+  t.context.db.getAdvertiser = () => { throw new Error() }
   const res = await t.context.app.inject({
     method: 'GET',
     url: '/advertiser/get',
