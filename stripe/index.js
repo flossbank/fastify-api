ec7889310f17cfd673690e0b3ef411c7f3c05498
Joel Wasserman <joel.wasserman8@gmail.com>
2020-02-05 11:37:42 -0800

    upgraded test coverage

--- stripe/index.js
+++ stripe/index.js
@@ -2,17 +2,19 @@ const fastifyPlugin = require('fastify-plugin')
 const { config } = require('../config')
 const stripe = require('stripe')(config.getStripeToken())
 
-function Stripe () {}
+function Stripe () {
+  this.stripe = stripe
+}
 
 Stripe.prototype.createStripeCustomer = async function createStripeCustomer (email) {
-  return stripe.customers.create({
+  return this.stripe.customers.create({
     email,
     metadata: { updatedAt: Date.now() }
   })
 }
 
 Stripe.prototype.updateStripeCustomer = async function updateStripeCustomer (customerId, sourceId) {
-  return stripe.customers.update(customerId, {
+  return this.stripe.customers.update(customerId, {
     source: sourceId,
     metadata: { updatedAt: Date.now() }
   })
