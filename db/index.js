cf56e792565522bdc2b18af141fbe3b4a026f1a5
Joel Wasserman <joel.wasserman8@gmail.com>
2020-03-02 13:51:56 -0800

    removed defaults in update advertiser card info

--- db/index.js
+++ db/index.js
@@ -86,7 +86,7 @@ Db.prototype.createAdvertiser = async function createAdvertiser (advertiser) {
   return insertedId
 }
 
-Db.prototype.updateAdvertiserHasCardInfo = async function updateAdvertiserHasCardInfo (id, hasCard = false, last4 = '') {
+Db.prototype.updateAdvertiserHasCardInfo = async function updateAdvertiserHasCardInfo (id, hasCard, last4) {
   return this.db.collection('advertisers').updateOne({
     _id: ObjectId(id)
   }, {
