8c03db11d6f6efe55f948dce664cbebf2166146f
Peter Squicciarini <stripedpajamas273@gmail.com>
2020-03-19 17:07:03 -0700

    Add unit tests for /user/check

--- schema/user/check.js
+++ schema/user/check.js
@@ -6,5 +6,13 @@ module.exports = {
       email: { type: 'string', maxLength: 128 },
       apiKey: { type: 'string', maxLength: 128 }
     }
+  },
+  response: {
+    200: {
+      type: 'object',
+      properties: {
+        success: { type: 'boolean' }
+      }
+    }
   }
 }
