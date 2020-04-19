0ed3586917db0285fc8d39dc7aa13b20e384e3ea
Peter Squicciarini <stripedpajamas273@gmail.com>
2020-04-18 21:25:01 -0700

    Bugfix cached API key

--- api/user/validate-captcha.js
+++ api/user/validate-captcha.js
@@ -18,7 +18,7 @@ module.exports = async (req, res, ctx) => {
     }
 
     const { insertedId, apiKey } = await ctx.db.createUser({ email, billingInfo: {} })
-    await ctx.auth.cacheApiKey(apiKey, insertedId)
+    await ctx.auth.cacheApiKey(apiKey, insertedId.toString())
     res.send({ success: true, apiKey })
   } catch (e) {
     ctx.log.error(e)
