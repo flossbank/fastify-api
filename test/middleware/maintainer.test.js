3fa9cf72489923dc08986f5f5103853c3f1f7e75
Peter Squicciarini <stripedpajamas273@gmail.com>
2020-06-22 22:06:55 -0700

    Add tests for middleware failure

---
+++ test/middleware/maintainer.test.js
@@ -0,0 +1,27 @@
+const test = require('ava')
+const { before, beforeEach, afterEach, after } = require('../_helpers/_setup')
+
+test.before(async (t) => {
+  await before(t)
+})
+
+test.beforeEach(async (t) => {
+  await beforeEach(t)
+  t.context.auth.maintainer.getWebSession = () => { throw new Error() }
+})
+
+test.afterEach(async (t) => {
+  await afterEach(t)
+})
+
+test.after.always(async (t) => {
+  await after(t)
+})
+
+test('GET `/maintainer/get` 500 middleware failure', async (t) => {
+  const res = await t.context.app.inject({
+    method: 'GET',
+    url: '/maintainer/get'
+  })
+  t.deepEqual(res.statusCode, 500)
+})
---
+++ test/middleware/userCli.test.js
@@ -0,0 +1,28 @@
+const test = require('ava')
+const { before, beforeEach, afterEach, after } = require('../_helpers/_setup')
+
+test.before(async (t) => {
+  await before(t)
+})
+
+test.beforeEach(async (t) => {
+  await beforeEach(t)
+  t.context.auth.user.getApiKey = () => { throw new Error() }
+})
+
+test.afterEach(async (t) => {
+  await afterEach(t)
+})
+
+test.after.always(async (t) => {
+  await after(t)
+})
+
+test('POST `/session/start` 500 middleware failure', async (t) => {
+  const res = await t.context.app.inject({
+    method: 'POST',
+    url: '/session/start',
+    payload: {}
+  })
+  t.deepEqual(res.statusCode, 500)
+})
