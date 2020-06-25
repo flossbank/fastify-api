2b278a9ad0cb9d8e5a208fb82a9428e02439ee69
Joel Wasserman <joel.wasserman8@gmail.com>
2020-06-25 12:33:11 -0700

    add ability to resume user web session

---
+++ api/user/resume.js
@@ -0,0 +1,24 @@
+const { MSGS: { INTERNAL_SERVER_ERROR } } = require('../../helpers/constants')
+
+// Resume should effectively "login" the user using just their auth token from their cookie
+// and supply the same return values that login does.
+module.exports = async (req, res, ctx) => {
+  try {
+    ctx.log.info('resuming user session for %s', req.session.userId)
+    const user = await ctx.db.user.get({ userId: req.session.userId })
+    if (!user) {
+      ctx.log.warn(
+        'attempt to resume session of non-existent user from %s',
+        req.session.userId
+      )
+      res.status(400)
+      return res.send({ success: false })
+    }
+    res.send({ success: true, user })
+  } catch (e) {
+    console.log('here', e)
+    ctx.log.error(e)
+    res.status(500)
+    res.send({ success: false, message: INTERNAL_SERVER_ERROR })
+  }
+}
---
+++ test/api/user/resume.test.js
@@ -0,0 +1,79 @@
+const test = require('ava')
+const { before, beforeEach, afterEach, after } = require('../../_helpers/_setup')
+const { USER_WEB_SESSION_COOKIE } = require('../../../helpers/constants')
+
+test.before(async (t) => {
+  await before(t, async ({ db, auth }) => {
+    const { id: userId1 } = await db.user.create({ email: 'honey@etsy.com' })
+    t.context.userId = userId1.toHexString()
+    t.context.sessionId = await auth.user.createWebSession({ userId: t.context.userId })
+  })
+})
+
+test.beforeEach(async (t) => {
+  await beforeEach(t)
+})
+
+test.afterEach(async (t) => {
+  await afterEach(t)
+})
+
+test.after(async (t) => {
+  await after(t)
+})
+
+test('GET `/user/resume` 401 unauthorized | no session', async (t) => {
+  const res = await t.context.app.inject({
+    method: 'GET',
+    url: '/user/resume',
+    headers: {
+      cookie: `${USER_WEB_SESSION_COOKIE}=not_a_gr8_cookie`
+    }
+  })
+  t.deepEqual(res.statusCode, 401)
+})
+
+test('GET `/user/resume` 200 | success', async (t) => {
+  const res = await t.context.app.inject({
+    method: 'GET',
+    url: '/user/resume',
+    headers: {
+      cookie: `${USER_WEB_SESSION_COOKIE}=${t.context.sessionId}`
+    }
+  })
+  t.deepEqual(res.statusCode, 200)
+  const payload = JSON.parse(res.payload)
+  t.deepEqual(payload.success, true)
+  const userRetrieved = await t.context.db.user.get({
+    userId: t.context.userId
+  })
+  t.deepEqual(payload.user, { 
+    id: userRetrieved.id.toHexString(),
+    billingInfo: userRetrieved.billingInfo,
+    email: userRetrieved.email
+  })
+})
+
+test('GET `/user/resume` 400 | no user', async (t) => {
+  t.context.db.user.get = () => undefined
+  const res = await t.context.app.inject({
+    method: 'GET',
+    url: '/user/resume',
+    headers: {
+      cookie: `${USER_WEB_SESSION_COOKIE}=${t.context.sessionId}`
+    }
+  })
+  t.deepEqual(res.statusCode, 400)
+})
+
+test('GET `/user/resume` 500 | user query error', async (t) => {
+  t.context.db.user.get = () => { throw new Error() }
+  const res = await t.context.app.inject({
+    method: 'GET',
+    url: '/user/resume',
+    headers: {
+      cookie: `${USER_WEB_SESSION_COOKIE}=${t.context.sessionId}`
+    }
+  })
+  t.deepEqual(res.statusCode, 500)
+})
