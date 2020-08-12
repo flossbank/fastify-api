a3b8d249ffa4f0e669320442f807bddb2a2e26dd
Joel Wasserman <joel.wasserman8@gmail.com>
2020-08-12 13:57:23 -0700

    just set up basic route endpoint for user github auth

---
+++ api/user/github-auth.js
@@ -0,0 +1,14 @@
+const { MSGS: { INTERNAL_SERVER_ERROR } } = require('../../helpers/constants')
+
+module.exports = async (req, res, ctx) => {
+  try {
+    const { code, state } = req.body
+    ctx.log.info('github auth requested with code %s and state %s', code, state)
+
+    res.send({ success: true })
+  } catch (e) {
+    ctx.log.error(e)
+    res.status(500)
+    res.send({ success: false, message: INTERNAL_SERVER_ERROR })
+  }
+}
