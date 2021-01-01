9fdd3d80f83d84606d403def09cfe63a3a7b3c54
Peter Squicciarini <stripedpajamas273@gmail.com>
2020-12-31 17:35:56 -0800

    Support partial matches when searching orgs by name

--- api/organization/get-org-by-name.js
+++ api/organization/get-org-by-name.js
@@ -3,28 +3,11 @@ const { MSGS: { INTERNAL_SERVER_ERROR } } = require('../../helpers/constants')
 module.exports = async (req, res, ctx) => {
   try {
     const { name, host } = req.query
-    ctx.log.info('finding org with name %s', name)
+    ctx.log.info('finding org with name %s, host %s', name, host)
 
-    if (!name) {
-      res.status(404)
-      return res.send({ success: false })
-    }
+    const organizations = await ctx.db.organization.searchByNameAndHost({ name, host })
 
-    let org = await ctx.db.organization.getByNameAndHost({ name, host })
-    if (!org) {
-      res.status(404)
-      return res.send({ success: false })
-    }
-
-    // Strip off private info from the org
-    org = {
-      name: org.name,
-      id: org.id,
-      globalDonation: org.globalDonation,
-      donationAmount: org.donationAmount,
-      avatarUrl: org.avatarUrl
-    }
-    res.send({ success: true, organization: org })
+    res.send({ success: true, organizations })
   } catch (e) {
     ctx.log.error(e)
     res.status(500)
