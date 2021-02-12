56884967b93b92870f4c99497e67f8fd662c9aa8
Joel Wasserman <joel.wasserman8@gmail.com>
2021-02-12 10:28:05 -0800

    remove unused endpoint

--- db/maintainer.js
+++ db/maintainer.js
@@ -13,16 +13,6 @@ class MaintainerDbController {
     return pkgs.map(({ _id: id, ...rest }) => ({ id, ...rest }))
   }
 
-  async getRevenue ({ maintainerId }) {
-    const packages = await this.db.collection('packages').find({
-      maintainers: { $elemMatch: { maintainerId } }
-    }).toArray()
-    return packages.reduce((totalRevenue, pkg) => {
-      const { revenuePercent } = pkg.maintainers.find((maintainer) => maintainer.maintainerId === maintainerId)
-      return totalRevenue + (pkg.totalRevenue * (revenuePercent / 100))
-    }, 0)
-  }
-
   async updateIlpPointer ({ userId, ilpPointer }) {
     return this.db.collection('users').updateOne({
       _id: ObjectId(userId)
