89901ced77e3099dad3bc1f252f4307c8e22af07
Joel Wasserman <joel.wasserman8@gmail.com>
2021-02-12 10:18:14 -0800

    add get pending payout endpoint

---
+++ api/maintainer/get-pending-payout.js
@@ -0,0 +1,23 @@
+const { MSGS: { INTERNAL_SERVER_ERROR } } = require('../../helpers/constants')
+
+module.exports = async (req, res, ctx) => {
+  try {
+    ctx.log.info('fetching pending payout for %s', req.session.userId)
+
+    let payout = 0
+    const payoutRes = await ctx.db.maintainer.getPendingPayout({
+      maintainerId: req.session.userId
+    })
+
+    if (payoutRes.length) {
+      // Round payout to the nearest 10 cents to obfuscate ad revenue abuse mechanism
+      payout = payoutRes.reduce((total, { payout: p }) => total + p, 0).toFixed(1)
+    }
+
+    res.send({ success: true, payout })
+  } catch (e) {
+    ctx.log.error(e)
+    res.status(500)
+    res.send({ success: false, message: INTERNAL_SERVER_ERROR })
+  }
+}
--- db/maintainer.js
+++ db/maintainer.js
@@ -30,6 +30,94 @@ class MaintainerDbController {
       $set: { 'payoutInfo.ilpPointer': ilpPointer }
     })
   }
+
+  // @returns an array of [{ _id: <packageId>, payout: <payoutInMS> }]
+  async getPendingPayout ({ maintainerId }) {
+    return this.db.collection('packages').aggregate([
+      {
+        $match: {
+          'maintainers.userId': {
+            $eq: maintainerId
+          }
+        }
+      }, {
+        $match: {
+          $or: [
+            {
+              adRevenue: {
+                $ne: null
+              }
+            }, {
+              donationRevenue: {
+                $ne: null
+              }
+            }
+          ]
+        }
+      }, {
+        $project: {
+          adRevenue: {
+            $filter: {
+              input: '$adRevenue',
+              as: 'adRev',
+              cond: {
+                $ne: [
+                  '$$adRev.paid', true
+                ]
+              }
+            }
+          },
+          donationRevenue: {
+            $filter: {
+              input: '$donationRevenue',
+              as: 'donRev',
+              cond: {
+                $ne: [
+                  '$$donRev.paid', true
+                ]
+              }
+            }
+          },
+          maintainers: 1
+        }
+      }, {
+        $project: {
+          adRevenue: {
+            $sum: '$adRevenue.amount'
+          },
+          donationRevenue: {
+            $sum: '$donationRevenue.amount'
+          },
+          maintainers: 1
+        }
+      }, {
+        $unwind: {
+          path: '$maintainers',
+          preserveNullAndEmptyArrays: false
+        }
+      }, {
+        $match: {
+          'maintainers.userId': maintainerId
+        }
+      }, {
+        $project: {
+          payout: {
+            $sum: [
+              {
+                $multiply: [
+                  '$adRevenue', '$maintainers.revenuePercent', 0.01
+                ]
+              }, {
+                $multiply: [
+                  '$donationRevenue', '$maintainers.revenuePercent', 0.01
+                ]
+              }
+            ]
+          }
+        }
+      }
+    ]).toArray()
+  }
 }
 
 module.exports = MaintainerDbController
