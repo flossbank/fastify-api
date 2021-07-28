8ac10dfd631808987611a8e529ab26e6ed92efc0
Joel Wasserman <joel.wasserman8@gmail.com>
2021-07-28 12:47:35 -0700

    add date to logs

--- api/user/get-installed-packages.js
+++ api/user/get-installed-packages.js
@@ -2,11 +2,11 @@ const { MSGS: { INTERNAL_SERVER_ERROR } } = require('../../helpers/constants')
 
 module.exports = async (req, res, ctx) => {
   try {
-    console.log('starging get installed packages request here')
+    console.log('starging get installed packages request here', Date.now())
     const installedPackages = await ctx.db.package.getUserInstalledPackages({ userId: req.session.userId })
-    console.log('have installed packages result here')
+    console.log('have installed packages result here', Date.now())
 
-    console.log('sending response here')
+    console.log('sending response here', Date.now())
     res.send({
       success: true,
       installedPackages
