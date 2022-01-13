8322a0d3f0e7dcb1722b62925c47f67f0b7d72b4
korlando7 <korlando7@gmail.com>
2022-01-12 17:50:00 -0800

    set username as email from payload, update tests

--- test/api/user/verify-registration.test.js
+++ test/api/user/verify-registration.test.js
@@ -43,31 +43,32 @@ test('POST `/user/verify-registration` 400 bad request', async (t) => {
 
 test('POST `/user/verify-registration` 200 success', async (t) => {
   const { db, auth } = t.context
+  const email = 'email@asdf.com'
 
-  const { registrationToken } = await auth.user.beginRegistration({ email: 'email@asdf.com' })
+  const { registrationToken } = await auth.user.beginRegistration({ email })
   auth.isRecaptchaResponseValid = () => true
 
   const res = await t.context.app.inject({
     method: 'POST',
     url: '/user/verify-registration',
-    payload: { email: 'email@asdf.com', token: registrationToken, recaptchaResponse: 'big messy' }
+    payload: { email, token: registrationToken, recaptchaResponse: 'big messy' }
   })
 
-  const user = await db.user.getByEmail({ email: 'email@asdf.com' })
+  const user = await db.user.getByEmail({ email })
 
+  t.deepEqual(user.username, email)
   t.true(res.headers['set-cookie'].includes(USER_WEB_SESSION_COOKIE))
   t.deepEqual(res.statusCode, 200)
   t.deepEqual(JSON.parse(res.payload), {
     success: true,
     user: {
-      username: '',
+      username: email,
       billingInfo: user.billingInfo,
       id: user.id.toString(),
       email: user.email
     }
   })
   t.true(user.apiKey.length > 0)
-  t.is(user.username, null)
 
   const apiKeyInfo = await auth.user.getApiKey({ apiKey: user.apiKey })
   t.is(apiKeyInfo.id, user.id.toString())
@@ -75,24 +76,26 @@ test('POST `/user/verify-registration` 200 success', async (t) => {
 
 test('POST `/user/verify-registration` 200 success | with referral code', async (t) => {
   const { db, auth } = t.context
+  const email = 'email_ref@asdf.com'
 
-  const { registrationToken } = await auth.user.beginRegistration({ email: 'email_ref@asdf.com', referralCode: 'papajohn' })
+  const { registrationToken } = await auth.user.beginRegistration({ email, referralCode: 'papajohn' })
   auth.isRecaptchaResponseValid = () => true
 
   const res = await t.context.app.inject({
     method: 'POST',
     url: '/user/verify-registration',
-    payload: { email: 'email_ref@asdf.com', token: registrationToken, recaptchaResponse: 'big messy' }
+    payload: { email, token: registrationToken, recaptchaResponse: 'big messy' }
   })
 
-  const user = await db.user.getByEmail({ email: 'email_ref@asdf.com' })
+  const user = await db.user.getByEmail({ email })
 
+  t.deepEqual(user.username, email)
   t.true(res.headers['set-cookie'].includes(USER_WEB_SESSION_COOKIE))
   t.deepEqual(res.statusCode, 200)
   t.deepEqual(JSON.parse(res.payload), {
     success: true,
     user: {
-      username: '',
+      username: email,
       billingInfo: user.billingInfo,
       id: user.id.toString(),
       email: user.email
@@ -100,10 +103,37 @@ test('POST `/user/verify-registration` 200 success | with referral code', async 
   })
   t.true(user.apiKey.length > 0)
   t.is(user.referralCode, 'papajohn')
-  t.is(user.username, null)
 
   const apiKeyInfo = await auth.user.getApiKey({ apiKey: user.apiKey })
   t.is(apiKeyInfo.id, user.id.toString())
+})
+
+test('POST `/user/verify-registration` 200 success | multiple sign ups', async (t) => {
+  const { auth } = t.context
+  const emails = ['email1@asdf.com', 'email2@asdf.com']
+
+  let { registrationToken } = await auth.user.beginRegistration({ email: emails[0] })
+  auth.isRecaptchaResponseValid = () => true
+
+  let res = await t.context.app.inject({
+    method: 'POST',
+    url: '/user/verify-registration',
+    payload: { email: emails[0], token: registrationToken, recaptchaResponse: 'big messy' }
+  })
+
+  t.deepEqual(res.statusCode, 200)
+
+  const registration = await auth.user.beginRegistration({ email: emails[1] })
+  registrationToken = registration.registrationToken
+  auth.isRecaptchaResponseValid = () => true
+
+  res = await t.context.app.inject({
+    method: 'POST',
+    url: '/user/verify-registration',
+    payload: { email: emails[1], token: registrationToken, recaptchaResponse: 'big messy' }
+  })
+
+  t.deepEqual(res.statusCode, 200)
 })
 
 test('POST `/user/verify-registration` 401 unauthorized', async (t) => {
